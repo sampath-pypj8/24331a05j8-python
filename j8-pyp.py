f4a0c61213a9f27967b1bbfe23661b635b3ae8f4
sampath <24331a05j8@mvgrce.edu.in>
2025-12-12 14:19:05 +0530

    this is my second file

---
+++ j8-pyp.py
@@ -0,0 +1,6 @@
+
+principal=int(input ("principal"))
+time=int(input ("time"))
+intrest=float(input ("intrest"))
+val =(principal*time*intrest)/100
+print("the simple intrest is " , val)
---
+++ j8pyp-3.py
@@ -0,0 +1,4 @@
+import math
+radius=int(input ("radius"))
+area=math.pi*radius*radius
+print ("the area of circle is :",area)
